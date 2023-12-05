a7c948f958128a96ea1b65ac99347b390aae8957
Mahran-Abdelati <mahran.abdelati@viacesi.fr>
2023-12-05 15:23:34 +0100

    Implémentation du composant Employé (CLmapEMP.h et CLmapEMP.cpp), code qui compile ne pasoublier de compléter les return des methodes avec les requêtes SQL adéquates.

--- Projet_POO_Groupe6/CLmapEMP.h
+++ Projet_POO_Groupe6/CLmapEMP.h
@@ -1,5 +1,41 @@
 #pragma once
-ref class CLmapEMP
+namespace NS_Comp
 {
-};
-
+	ref class CLmapEMP
+	{
+	private :
+		//System::String^ sSql;
+		int Id;
+		System::String^ lastName;
+		System::String^ firstName;
+		int streetNumber;
+		System::String^ streetName;
+		System::String^ ZipCode;
+		System::String^ cityName;
+		System::String^ hireDate;
+		System::String^ ChiefID;
+	public :
+		System::String^ selectEmployees(void);
+		System::String^ insertEmployee(void);
+		System::String^ deleteEmployee(void);
+		System::String^ updateEmployee(void);
+		void setId(int id);
+		void setLastName(System::String^ lastName);
+		void setFirstName(System::String^ firstName);
+		void setStreetNumber(int streetNumber);
+		void setStreetName(System::String^ streetName);
+		void setZipCode(System::String^ ZipCode);
+		void setCityName(System::String^ CityName);
+		void setHireName(System::String^ HireDate);
+		void setChiefId(System::String^ ChiefId);
+		int getId(void);
+		System::String^ getLastName(void);
+		System::String^ getFirstName(void);
+		int getStreetNumber(void);
+		System::String^ getStreetName(void);
+		System::String^ getZipCode(void);
+		System::String^ getCityName(void);
+		System::String^ getHireDate(void);
+		System::String^ getChiefId(void);
+	};
+}
