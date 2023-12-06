63108926fdd4f296ba7c8d43ea912afaf481560e
Brieuc OLIVIERI <brieuc.olivieri@viacesi.fr>
2023-12-06 11:39:59 +0100

    Implementation of the CLmapPRODUCT component with all the getters and setters. Only missing the CRUDs.

--- Projet_POO_Groupe6/CLmapPRODUCT.cpp
+++ Projet_POO_Groupe6/CLmapPRODUCT.cpp
@@ -1,2 +1,75 @@
 #include "pch.h"
 #include "CLmapPRODUCT.h"
+
+System::String^ NS_Comp::CLmapPRODUCT::selectProduct(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insert return statement here
+}
+
+System::String^ NS_Comp::CLmapPRODUCT::insertProduct(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insert return statement here
+}
+
+System::String^ NS_Comp::CLmapPRODUCT::deleteProduct(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insert return statement here
+}
+
+System::String^ NS_Comp::CLmapPRODUCT::updateProduct(void)
+{
+    throw gcnew System::NotImplementedException();
+    // TODO: insert return statement here
+}
+
+void NS_Comp::CLmapPRODUCT::setProductRef(int pref)
+{
+    this->productRef = pref;
+}
+
+void NS_Comp::CLmapPRODUCT::setProductName(System::String^ pname)
+{
+    this->productName = pname;
+}
+
+void NS_Comp::CLmapPRODUCT::setPriceET(float pET)
+{
+    this->PriceET = pET;
+}
+
+void NS_Comp::CLmapPRODUCT::setStockQuantity(int sq)
+{
+    this->stockQuantity = sq;
+}
+
+void NS_Comp::CLmapPRODUCT::setReorderThreshold(int rTh)
+{
+    this->reorderThreshold = rTh;
+}
+
+void NS_Comp::CLmapPRODUCT::setVATrate(float VATr)
+{
+    this->VATrate = VATr;
+}
+
+void NS_Comp::CLmapPRODUCT::setColor(System::String^ color)
+{
+    this->color = color;
+}
+
+int NS_Comp::CLmapPRODUCT::getProductRef(void) { return this->productRef;  }
+
+System::String^ NS_Comp::CLmapPRODUCT::getProductName(void) { return this->productName;  }
+
+float NS_Comp::CLmapPRODUCT::getPriceET(void) { return this->PriceET;  }
+
+int NS_Comp::CLmapPRODUCT::getStockQuantity(void) { return this->stockQuantity; }
+
+int NS_Comp::CLmapPRODUCT::getReorderThreshold(void) { return this->reorderThreshold; }
+
+float NS_Comp::CLmapPRODUCT::getVATrate(void) { return this->VATrate; }
+
+System::String^ NS_Comp::CLmapPRODUCT::getColor(void) { return this->color; }
--- Projet_POO_Groupe6/CLmapPRODUCT.h
+++ Projet_POO_Groupe6/CLmapPRODUCT.h
@@ -1,5 +1,37 @@
 #pragma once
-ref class CLmapPRODUCT
-{
-};
 
+namespace NS_Comp
+{
+	ref class CLmapPRODUCT
+	{
+	private:
+		System::String^ sSql;
+		int productRef;
+		System::String^ productName;
+		float PriceET;
+		int stockQuantity;
+		int reorderThreshold;
+		float VATrate;
+		System::String^ color;
+
+	public:
+		System::String^ selectProduct(void);
+		System::String^ insertProduct(void);
+		System::String^ deleteProduct(void);
+		System::String^ updateProduct(void);
+		void setProductRef(int);
+		void setProductName(System::String^);
+		void setPriceET(float);
+		void setStockQuantity(int);
+		void setReorderThreshold(int);
+		void setVATrate(float);
+		void setColor(System::String^);
+		int getProductRef(void);
+		System::String^ getProductName(void);
+		float getPriceET(void);
+		int getStockQuantity(void);
+		int getReorderThreshold(void);
+		float getVATrate(void);
+		System::String^ getColor(void);
+	};
+}
