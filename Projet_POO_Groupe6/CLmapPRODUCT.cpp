fc511d4327bfc9774d38624d0a9ed54309bb82d1
Mahran-Abdelati <mahran.abdelati@viacesi.fr>
2023-12-08 18:52:47 +0100

    Codage des boutons update et add du stock il restera le delete à faire, code qui compile mais quelques erreurs surviennent de mon coté

--- Projet_POO_Groupe6/CLmapPRODUCT.cpp
+++ Projet_POO_Groupe6/CLmapPRODUCT.cpp
@@ -3,30 +3,56 @@
 
 System::String^ NS_Comp::CLmapPRODUCT::selectProducts(void)
 {
-    return "SELECT [Projet_POO_G6].[dbo].[Products].[productRef], [productName], [color], [priceET], [stockQuantity], [reorderThreshold], [VATrate] from [Projet_POO_G6].[dbo].[Products] \
+    return "SELECT [Projet_POO_G6].[dbo].[characteristicsProd].[colorProductID], [Projet_POO_G6].[dbo].[Products].[productRef], [productName], [color], [priceET], [stockQuantity], [reorderThreshold], [VATrate] from [Projet_POO_G6].[dbo].[Products] \
             full join [Projet_POO_G6].[dbo].[characteristicsProd] on ([Projet_POO_G6].[dbo].[Products].[productRef] = [Projet_POO_G6].[dbo].[characteristicsProd].[productRef]);";
 }
 
 System::String^ NS_Comp::CLmapPRODUCT::selectProduct(void)
 {
-    return "SELECT [Projet_POO_G6].[dbo].[Products].[productRef], [productName], [color], [priceET], [stockQuantity], [reorderThreshold], [VATrate] from [Projet_POO_G6].[dbo].[Products] \
+    return "SELECT [Projet_POO_G6].[dbo].[characteristicsProd].[colorProductID], [Projet_POO_G6].[dbo].[Products].[productRef], [productName], [color], [priceET], [stockQuantity], [reorderThreshold], [VATrate] from [Projet_POO_G6].[dbo].[Products] \
             full join [Projet_POO_G6].[dbo].[characteristicsProd] on ([Projet_POO_G6].[dbo].[Products].[productRef] = [Projet_POO_G6].[dbo].[characteristicsProd].[productRef]) \
-            WHERE [Projet_POO_G6].[dbo].[Products].[productRef] = '" + productRef + "';";
+            WHERE [Projet_POO_G6].[dbo].[characteristicsProd].[colorProductID] = '" + productRef + "';";
 }
 
 System::String^ NS_Comp::CLmapPRODUCT::insertProduct(void)
 {
-    return ""; //Compléter avec la bonne requete SQL
+    return "DECLARE @productName VARCHAR(50) = '" + productName + "'; \
+            DECLARE @color CHAR(12) = '" + color + "'; \
+            DECLARE @stockQuantity INT = '" + stockQuantity + "'; \
+            DECLARE @reorderThreshold INT = '" + reorderThreshold + "'; \
+            DECLARE @priceET DECIMAL(15, 5) = '" + PriceET + "'; \
+            DECLARE @VATrate DECIMAL(3, 1) = '" + VATrate + "'; \
+            INSERT INTO [Projet_POO_G6].[dbo].[Products]([productName], [priceET], [reorderThreshold], [VATrate]) \
+            VALUES(@productName, @priceET, @reorderThreshold, @VATrate); \
+            DECLARE @productRef INT; \
+            SET @productRef = SCOPE_IDENTITY(); \
+            INSERT INTO [Projet_POO_G6].[dbo].[characteristicsProd]([stockQuantity], [color], [productRef]) \
+            VALUES(@stockQuantity, @color, @productRef); ";
 }
 
 System::String^ NS_Comp::CLmapPRODUCT::deleteProduct(void)
 {
-    return ""; //Compléter avec la bonne requete SQL
+    return "";
 }
 
 System::String^ NS_Comp::CLmapPRODUCT::updateProduct(void)
 {
-    return ""; //Compléter avec la bonne requete SQL
+    System::String^ query = "DECLARE @productRef INT;"
+                            "SET @productRef = (SELECT [productRef] FROM [Projet_POO_G6].[dbo].[characteristicsProd] WHERE [colorProductID] = '" + productRef + "');"
+                            "UPDATE[Projet_POO_G6].[dbo].[Products]"
+                            "SET"
+                            "[productName] = '" + productName + "',"
+                            "[VATrate] = '" + VATrate + "',"
+                            "[reorderThreshold] = '" + reorderThreshold + "'"
+                            "WHERE"
+                            "[productRef] = @productRef;"
+                            "UPDATE[Projet_POO_G6].[dbo].[characteristicsProd]"
+                            "SET"
+                            "[color] = '" + color + "',"
+                            "[stockQuantity] = '" + stockQuantity + "'"
+                            "WHERE"
+                            "[colorProductID] = '" + productRef + "';";
+        return query;
 }
 
 void NS_Comp::CLmapPRODUCT::setProductRef(int pref)
